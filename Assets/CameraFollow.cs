b73095a131baab4618276847135bbe33b1009c9d
MacFan4000 <paulfkeffer@gmail.com>
2020-04-21 18:00:37 -0400

    update

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+ using System.Collections;
+ public class CameraFollow : MonoBehaviour {
+     public Transform target;
+     Camera mycam;
+ 
+     void Start()
+     {
+         mycam = GetComponent<Camera> ();
+ 
+     }
+     void Update()
+     {
+         if (target)
+         {
+             transform.position = Vector3.Lerp (transform.position, target.position, 0.1f) + new Vector3 (0, 0, -10);
+         }
+     }
+ }
